f4cf14a69c9c8e9f379b6ceb242fcdfa6d6e7168
weschweitzer@gmail.com <weschweitzer@gmail.com>
2024-09-24 15:36:42 -0400

    Initial Commit

---
+++ LastStandDefenseTests/LastStandDefenseTests.swift
@@ -0,0 +1,17 @@
+//
+//  LastStandDefenseTests.swift
+//  LastStandDefenseTests
+//
+//  Created by Will Schweitzer on 9/24/24.
+//
+
+import Testing
+@testable import LastStandDefense
+
+struct LastStandDefenseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
